9d3f25653add62365254557622b864ae0d252ac3
Ahmad Masri <amsm7437@gmail.com>
2025-02-25 22:26:15 +0300

    AppBar

--- project1/src/pages/servivesPage.jsx
+++ project1/src/pages/servivesPage.jsx
@@ -4,7 +4,7 @@ const ServicesPage = () => {
   return (
     <>
       <div style={{ width: "100%", height: "100dvh", position: "relative" }}>
-        <img className="img1" src={"/images/services1.png"} />
+        <img className="img1" src={"../public/images/services1.png"} />
       </div>
     </>
   );
