81ec921157ac5c01f29ff2f5a61b74a1f836f0c8
Arbirt <mohamadwkudr@gmail.com>
2025-02-26 00:24:54 +0300

    main page is done

--- project1/src/pages/servivesPage.jsx
+++ project1/src/pages/servivesPage.jsx
@@ -2,6 +2,24 @@ import AppBar from "../components/AppBar";
 import "../styles/servicesPageStyle.css";
 
 const ServicesPage = () => {
+  const cards = [
+    {
+      p1: "AI-Powered Precision in Emergencies",
+      p2: "Hyper-specialized AI agents, faster than human limits.",
+    },
+    {
+      p1: "Real-Time Decision Support",
+      p2: "From chaos to clarity, instantly.",
+    },
+    {
+      p1: "Error-Reducing Intelligence",
+      p2: "High level Ai with little to no errors",
+    },
+    {
+      p1: "Dynamic Learning for Syria's Needs",
+      p2: "From chaos to clarity, instantly.",
+    },
+  ];
   return (
     <>
       <div
@@ -71,138 +89,45 @@ const ServicesPage = () => {
             </div>
           </div>
           <div className="services-cards">
-            <div className="cards">
-              <div style={{ width: 46, height: 46, position: "relative" }}>
-                <div
-                  style={{
-                    width: 46,
-                    height: 46,
-                    left: 0,
-                    top: 0,
-                    position: "absolute",
-                    background: "rgba(255, 255, 255, 0.64)",
-                    borderRadius: 9999,
-                  }}
-                ></div>
-                <div
-                  style={{
-                    width: 24,
-                    height: 24,
-                    left: 11,
-                    top: 11,
-                    position: "absolute",
-                    borderRadius: 25,
-                  }}
-                >
-                  <div
-                    style={{
-                      width: 12,
-                      height: 12,
-                      left: 6,
-                      top: 6,
-                      position: "absolute",
-                      borderRadius: 3,
-                      border: "2px #242E49 solid",
-                    }}
-                  ></div>
-                  <div
-                    data-svg-wrapper
-                    style={{ left: "12px", top: "2px", position: "absolute" }}
-                  >
-                    <svg width="2" height="4" viewBox="0 0 2 4" fill="none">
-                      <path d="M1 4V0" stroke="#242E49" strokeWidth="2" />
-                    </svg>
-                  </div>
-                  <div
-                    data-svg-wrapper
-                    style={{ left: "12px", top: "22px", position: "absolute" }}
-                  >
-                    <svg width="2" height="4" viewBox="0 0 2 4" fill="none">
-                      <path d="M1 0V4" stroke="#242E49" strokeWidth="2" />
-                    </svg>
-                  </div>
-                  <div
-                    data-svg-wrapper
-                    style={{ left: "12px", top: "12px", position: "absolute" }}
-                  >
-                    <svg width="4" height="2" viewBox="0 0 4 2" fill="none">
-                      <path d="M4 1L0 1" stroke="#242E49" strokeWidth="2" />
-                    </svg>
-                  </div>
-                  <div
-                    data-svg-wrapper
-                    style={{ left: "22px", top: "12px", position: "absolute" }}
-                  >
-                    <svg width="4" height="2" viewBox="0 0 4 2" fill="none">
-                      <path d="M0 1L4 1" stroke="#242E49" strokeWidth="2" />
-                    </svg>
-                  </div>
-                  <div
-                    data-svg-wrapper
-                    style={{ left: "4px", top: "4px", position: "absolute" }}
-                  >
-                    <svg width="5" height="5" viewBox="0 0 5 5" fill="none">
-                      <path
-                        d="M3.75738 3.75738L1 1"
-                        stroke="#242E49"
-                        strokeWidth="2"
-                      />
-                    </svg>
-                  </div>
-                  <div
-                    data-svg-wrapper
-                    style={{ left: "20px", top: "20px", position: "absolute" }}
-                  >
-                    <svg width="5" height="5" viewBox="0 0 5 5" fill="none">
-                      <path
-                        d="M1.24262 1.24262L4 4"
-                        stroke="#242E49"
-                        strokeWidth="2"
-                      />
-                    </svg>
-                  </div>
-                  <div
-                    data-svg-wrapper
-                    style={{ left: "4px", top: "20px", position: "absolute" }}
-                  >
-                    <svg width="5" height="5" viewBox="0 0 5 5" fill="none">
-                      <path
-                        d="M3.75738 1.24262L1 4"
-                        stroke="#242E49"
-                        strokeWidth="2"
-                      />
-                    </svg>
-                  </div>
-                  <div
-                    data-svg-wrapper
-                    style={{ left: "20px", top: "4px", position: "absolute" }}
-                  >
-                    <svg width="5" height="5" viewBox="0 0 5 5" fill="none">
-                      <path
-                        d="M1.24262 3.75738L4 1"
-                        stroke="#242E49"
-                        strokeWidth="2"
-                      />
-                    </svg>
-                  </div>
-                  <div
-                    data-svg-wrapper
-                    style={{ left: "10px", top: "10px", position: "absolute" }}
-                  >
-                    <svg width="6" height="6" viewBox="0 0 6 6" fill="none">
-                      <path
-                        d="M1 2.5V3.5C1 4.32843 1.67157 5 2.5 5H3.5C4.32843 5 5 4.32843 5 3.5V2.5C5 1.67157 4.32843 1 3.5 1H2.5C1.67157 1 1 1.67157 1 2.5Z"
-                        stroke="#242E49"
-                        strokeWidth="2"
-                      />
-                    </svg>
+            {cards.map((card, index) => (
+              <div key={index} className="cards">
+                <div style={{ width: "46px", height: "46px", flexShrink: 0 }}>
+                  <div style={{ width: 46, height: 46, position: "relative" }}>
+                    {/* Outer Circle */}
+                    <div
+                      style={{
+                        width: 46,
+                        height: 46,
+                        left: 0,
+                        top: 0,
+                        position: "absolute",
+                        background: "rgba(255, 255, 255, 0.64)",
+                        zIndex: 10,
+                        borderRadius: 9999,
+                      }}
+                    />
+                    <img
+                      src="../public/images/virus.svg"
+                      alt="My Icon"
+                      width="32"
+                      height="32"
+                      style={{
+                        zIndex: 1000,
+                        position: "absolute",
+                        color: "black",
+                        top: 7,
+                        left: 7,
+                      }}
+                    />
                   </div>
                 </div>
+
+                <div className="cards-text">
+                  <p>{card.p1}</p>
+                  <p>{card.p2}</p>
+                </div>
               </div>
-            </div>
-            <div className="cards"></div>
-            <div className="cards"></div>
-            <div className="cards"></div>
+            ))}
           </div>
         </div>
       </div>
